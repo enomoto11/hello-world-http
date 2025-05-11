ac105b4f0533cb353696ed31245df7c26dfe699a
enomoto11 <enomoto_togo@cyberagent.co.jp>
2025-05-11 10:11:50 +0900

    lib.rsのfetch関数をmain関数に変更し、POSTリクエストの処理を追加しました。ファイルのアップロードをサポートし、リクエストメソッドの検証を行うようにしました。

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,27 @@
-use worker::{event, Body, Context, Env, HttpRequest, HttpResponse, Result};
+use worker::*;
 
 #[event(fetch)]
-async fn fetch(_req: HttpRequest, _env: Env, _ctx: Context) -> Result<HttpResponse> {
-    console_error_panic_hook::set_once();
-    Ok(http::Response::builder()
-        .status(http::StatusCode::OK)
-        .body(Body::empty())?)
+pub async fn main(mut req: Request, _env: Env, _ctx: worker::Context) -> Result<Response> {
+    console_log!(
+        "{} {}, located at: {:?}, within: {}",
+        req.method().to_string(),
+        req.path(),
+        req.cf().unwrap().coordinates().unwrap_or_default(),
+        req.cf().unwrap().region().unwrap_or("unknown region".into())
+    );
+
+    if !matches!(req.method(), Method::Post) {
+        return Response::error("Method Not Allowed", 405);
+    }
+
+    if let Some(file) = req.form_data().await?.get("file") {
+        return match file {
+            FormEntry::File(buf) => {
+                Response::ok(&format!("size = {}", buf.bytes().await?.len()))
+            }
+            _ => Response::error("`file` part of POST form must be a file", 400),
+        };
+    }
+
+    Response::error("Bad Request", 400)
 }
